753e06b2cbcafd113ef601b1519dfbb55839ea67
sahil <sahilasopa12@gmail.com>
2021-09-25 15:55:16 +0530

    Commit

--- app/src/main/java/com/sahilasopa/chat/LoginActivity.java
+++ app/src/main/java/com/sahilasopa/chat/LoginActivity.java
@@ -98,11 +98,11 @@ public class LoginActivity extends AppCompatActivity {
             try {
                 // Google Sign In was successful, authenticate with Firebase
                 GoogleSignInAccount account = task.getResult(ApiException.class);
-                Log.d("TAG", "firebaseAuthWithGoogle:" + account.getId());
+                Log.d("response", "firebaseAuthWithGoogle:" + account.getId());
                 firebaseAuthWithGoogle(account.getIdToken());
             } catch (ApiException e) {
                 // Google Sign In failed, update UI appropriately
-                Log.w("TAG", "Google sign in failed", e);
+                Log.w("response", "Google sign in failed", e);
             }
         }
     }
